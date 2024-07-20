fea260628310687ae3690e9a54c7f4c87cb8fd44
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-07-20 08:55:55 +0200

    update6

--- src/Components/Landing/Hero.jsx
+++ src/Components/Landing/Hero.jsx
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import HeroImg from '../../../public/Images/HeroImg.svg';
+import { IoPlayCircleOutline } from "react-icons/io5";
 import { IoMdArrowForward } from "react-icons/io";
 import 'aos/dist/aos.css';
+
 import AOS from 'aos';
 
 function Hero() {
@@ -28,7 +30,7 @@ function Hero() {
   }, []);
 
   return (
-    <div className='min-h-screen flex items-center justify-between md:w-4/5 m-auto font-pop'>
+    <div className='min-h-screen flex items-center justify-between md:w-4/5 m-auto font-poppins'>
       <div className='flex flex-col md:flex-row items-center p-2 md:p-0'>
         <div className='text-center mt-20 md:mt-0 md:text-left md:w-1/2' data-aos='fade-down'>
           <h1 className='text-4xl font-bold mb-4 text-[#404660]  pop'>
@@ -41,9 +43,13 @@ function Hero() {
             for Improved Performance
           </h1>
           <p className='text-lg mb-6 pop text-gray-500'>Sidec is a web app that allows students to practice and know how prepared they are for a National Exam.</p>
-          <button className='bg-[#9835ff] text-white px-6 py-3 rounded-lg font-normal text-lg transition-transform duration-300 hover:translate-y-[-3px] pop flex items-center gap-2 m-auto md:m-0'>
+          <div className='flex gap-4 items-center justify-center md:justify-start'>
+          <button className='bg-[#9835ff] text-white px-6 py-3 rounded-lg font-normal text-lg transition-transform duration-300 hover:translate-y-[-3px] pop flex items-center gap-2  md:m-0'>
             Join Waitlist <IoMdArrowForward />
           </button>
+          <button className='border border-[#9835ff] text-[#9835ff] px-6 py-3 rounded-lg font-normal text-lg transition-transform duration-300 hover:translate-y-[-3px] pop flex items-center gap-2  md:m-0'><IoPlayCircleOutline size={20} /> Demo</button>
+          </div>
+        
         </div>
         <div className='mt-8 md:mt-0 md:ml-8 md:w-1/2' data-aos='fade-right'>
           <img src={HeroImg} alt="Hero Image" className='w-full h-auto' />
--- src/Components/Landing/Navbar.jsx
+++ src/Components/Landing/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar() {
         <img src={Logo} alt="Logo" className=" w-full " />
             </div>
         
-        <div className="hidden md:flex space-x-8 text-[#404660] ">
+        <div className="hidden md:flex space-x-8 text-[#404660] font-poppins">
           <a href="#home" className=" border-b-2 border-white border-solid hover:border-[#9835ff]">Home</a>
           <a href="#about" className="border-b-2 border-white border-solid hover:border-[#9835ff]">About</a>
           <a href="#team" className="border-b-2 border-white border-solid hover:border-[#9835ff]">Team</a>
@@ -36,15 +36,15 @@ function Navbar() {
           <button className="bg-[#9835ff] text-white px-4 py-2 rounded hover:translate-y-[-3px] transition-all duration-300">Get Started</button>
         </div>
         <div className="md:hidden">
-          <button onClick={() => setIsDrawerOpen(true)} className="text-gray-800">
-            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <button onClick={() => setIsDrawerOpen(true)} className="text-[#404660]">
+            <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
             </svg>
           </button>
         </div>
       </div>
       {isDrawerOpen && (
-        <div className="fixed inset-0 bg-white z-40 flex flex-col items-center justify-center space-y-8">
+        <div className="fixed inset-0 bg-white z-40 flex flex-col items-center justify-center space-y-8 font-poppins">
           <button onClick={() => setIsDrawerOpen(false)} className="absolute top-4 right-4 text-gray-800">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
